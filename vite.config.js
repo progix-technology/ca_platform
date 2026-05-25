1b31eb1f3eff9ee4df827d7b3372b4a354fcb636
Vivang Mishra <your_email@example.com>
2026-05-25 13:07:08 +0530

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Electron ke liye relative paths zaroori hain
+  base: './',
+  build: {
+    outDir: 'dist', // <-- yahi hona chahiye, root/dist
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Aapka backend port
+      '/api': 'http://localhost:5000',
     },
   },
 })
